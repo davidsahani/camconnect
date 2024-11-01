3b0af5e0a26415bbaf29e7760ece8ebf74f35d24
David <97206601+davidsahani@users.noreply.github.com>
2024-11-01 14:59:46 +0530

    Updated build script

--- build.py
+++ build.py
@@ -1,41 +1,24 @@
 import os
 import sys
 import json
+import logging
 import shutil
 import subprocess
 from typing import Any
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-CAMCONNECT_MOBILE_DIR = os.path.join(SCRIPT_DIR, "camconnect")
-CAMCONNECT_DESKTOP_DIR = os.path.join(SCRIPT_DIR, "camconnect-desktop")
-
-CAMCONNECT_MOBILE_BUILD_DIR = os.path.join(CAMCONNECT_MOBILE_DIR, "build\\app\\outputs\\flutter-apk")
-CAMCONNECT_DESKTOP_BUILD_DIR = os.path.join(CAMCONNECT_DESKTOP_DIR, "build\\windows\\x64\\runner\\Release")
-
-PROJECT_OUTPUT_DIR = os.path.join(SCRIPT_DIR, "build")  # where build files will be copied to.
-CAMCONNECT_DESKTOP_OUTPUT_DIR = os.path.join(PROJECT_OUTPUT_DIR, "camconnect")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(message)s"
+)
 
 
-def copy_files(source_dir: str, destination_dir: str) -> None:
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
+def get_version_from_pubspec(file_path: str) -> str:
+    with open(file_path) as file:
+        for line in file:
+            if line.strip().startswith("version:"):
+                return line.split(":")[1].strip()
 
-    for dir_path in os.listdir(source_dir):
-        curr_path = os.path.join(source_dir, dir_path)
-
-        if os.path.isfile(curr_path):
-            shutil.copy(curr_path, destination_dir)
-            continue  # copy the file and continue.
-
-        # walk through the directory and copy the files.
-        for dirpath, _dirnames, filenames in os.walk(curr_path):
-            for filename in filenames:
-                source_path = os.path.join(dirpath, filename)
-                relative_path = os.path.relpath(source_path, source_dir)
-                destination_path = os.path.join(destination_dir, relative_path)
-                os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-                shutil.copy(source_path, destination_path)
+    raise ValueError(f"Version not found in {os.path.split(file_path)[1]}")
 
 
 def rename_files_in_directory(directory: str, old_prefix: str, new_prefix: str) -> None:
@@ -64,64 +47,144 @@ def run_flutter_clean(cwd: str) -> int:
     return subprocess.call(["flutter", "clean"], cwd=cwd, shell=True)
 
 
-def build_camconnect_mobile(version_name: str, version_code: int) -> int:
-    return subprocess.call(["flutter", "build", "apk", "--release", "--split-per-abi",
-            f"--build-name={version_name}", f"--build-number={version_code}"
-        ], cwd=CAMCONNECT_MOBILE_DIR, shell=True
+def build_camconnect_mobile(project_dir: str, output_dir: str) -> int:
+    release_dir = os.path.join(project_dir, "build\\app\\outputs\\flutter-apk")
+
+    if os.path.exists(output_dir):
+        logging.info("Cleaning previous build directory...")
+        clean_dir(output_dir)
+
+    logging.info("Building camconnect mobile...")
+    status = subprocess.call(
+        ["flutter", "build", "apk", "--release", "--split-per-abi"],
+        cwd=project_dir, shell=True
     )
 
-
-def build_camconnect_desktop(version_name: str, version_code: int) -> int:
-    return subprocess.call(["flutter", "build", "windows", "--release",
-            f"--build-name={version_name}", f"--build-number={version_code}"
-        ], cwd=CAMCONNECT_DESKTOP_DIR, shell=True
-    )
-
-
-def main() -> None:
-    config = open_config(os.path.join(SCRIPT_DIR, "versions.json"))
-
-    mobile_app_version: str = config["mobile"]["version"]
-    mobile_app_version_code: int = config["mobile"]["version_code"]
-
-    desktop_app_version: str = config["desktop"]["version"]
-    desktop_app_version_code: int = config["desktop"]["version_code"]
-
-    if os.path.exists(PROJECT_OUTPUT_DIR):
-        print("Cleaning previous build directory...")
-        clean_dir(PROJECT_OUTPUT_DIR)
-
-    print("Running flutter clean...")
-    status = run_flutter_clean(CAMCONNECT_MOBILE_DIR)
     if status != 0:
-        return  # don't continue if flutter clean failed.
+        logging.error("camconnect mobile build failed.")
+        return status
 
-    status = run_flutter_clean(CAMCONNECT_DESKTOP_DIR)
-    if status != 0:
-        return  # don't continue if flutter clean failed.
+    logging.info("Copying camconnect mobile files...")
+    shutil.copytree(release_dir, output_dir, dirs_exist_ok=True)
 
-    print("Building camconnect mobile...")
-    status = build_camconnect_mobile(mobile_app_version, mobile_app_version_code)
-
-    if status != 0:  # don't continue if mobile build failed.
-        return print("camconnect mobile build failed.", file=sys.stderr)
-
-    print("Copying camconnect mobile files...")
-    copy_files(CAMCONNECT_MOBILE_BUILD_DIR, PROJECT_OUTPUT_DIR)
+    logging.info("Renaming camconnect apk files prefixes...")
     # rename the files to replace the "app" prefix to "camconnect".
-    rename_files_in_directory(PROJECT_OUTPUT_DIR, "app", "camconnect")
+    rename_files_in_directory(output_dir, "app", "camconnect")
 
-    print("Building camconnect desktop...")
-    status = build_camconnect_desktop(desktop_app_version, desktop_app_version_code)
+    return status  # success.
 
-    if status != 0:  # don't continue if desktop build failed.
-        return print("camconnect desktop build failed.", file=sys.stderr)
 
-    print("Copying camconnect desktop files...")
-    copy_files(CAMCONNECT_DESKTOP_BUILD_DIR, CAMCONNECT_DESKTOP_OUTPUT_DIR)
+def build_camconnect_desktop(project_dir: str, output_dir: str) -> int:
+    release_dir = os.path.join(project_dir, "build\\windows\\x64\\runner\\Release")
 
-    print("Done building projects.")
+    if os.path.exists(output_dir):
+        logging.info("Cleaning previous build directory...")
+        clean_dir(output_dir)
+
+    logging.info("Running flutter clean camconnect desktop...")
+    status = run_flutter_clean(project_dir)
+    if status != 0:
+        return status # don't continue if flutter clean failed.
+
+    logging.info("Building camconnect desktop...")
+    status = subprocess.call(
+        ["flutter", "build", "windows", "--release"],
+        cwd=project_dir, shell=True
+    )
+
+    if status != 0:
+        logging.error("camconnect desktop build failed.")
+        return status
+
+    logging.info("Copying camconnect desktop files...")
+    shutil.copytree(release_dir, output_dir, dirs_exist_ok=True)
+
+    return status  # success.
+
+
+def create_setup_installer(script_file: str, app_version: str) -> int:
+    program_path = "C:\\Program Files (x86)"
+    inno_setup_dir = [pathname for pathname in os.listdir(
+        program_path) if pathname.lower().startswith("inno setup")
+    ]
+    if not inno_setup_dir:
+        logging.error(
+            f"Couldn't locate Inno Setup in '{program_path}' " +
+            "Either it's not installed or is in alternate path.",
+        )
+        return 1
+    iscc = os.path.join(program_path, inno_setup_dir[0], "ISCC.exe")
+    if not os.path.exists(iscc):
+        logging.error(
+            "Couldn't locate Inno Setup Complier (ISCC.exe) " +
+            f"in '{os.path.dirname(iscc)}'"
+        )
+        return 1
+
+    logging.info("Creating setup installer...")
+    # create setup installer using inno setup compiler.
+    return subprocess.call([iscc, script_file, f"/DAppVersion={app_version}"])
+
+
+def main() -> int:
+    script_dir = os.path.dirname(__file__)
+    output_dir = os.path.join(script_dir, "build")
+
+    mobile_project_dir = os.path.join(script_dir, "camconnect")
+    desktop_project_dir = os.path.join(script_dir, "camconnect-desktop")
+
+    mobile_output_dir = os.path.join(output_dir, "camconnect-apk")
+    desktop_output_dir = os.path.join(output_dir, "camconnect")
+
+    option = " ".join(sys.argv[1:]).lower() if len(sys.argv) > 1 else "all"
+
+    if option in ("clean mobile", "clean", "clean all"):
+        logging.info("Running flutter clean camconnect mobile...")
+        status = run_flutter_clean(mobile_project_dir)
+        if status != 0:
+            return status
+
+    if option in ("clean desktop", "clean", "clean all"):
+        logging.info("Running flutter clean camconnect desktop...")
+        status = run_flutter_clean(desktop_project_dir)
+        if status != 0:
+            return status
+
+    if option == "mobile" or option == "all":
+        status = build_camconnect_mobile(mobile_project_dir, mobile_output_dir)
+        if status != 0:
+            return status
+
+    if option == "desktop" or option == "all":
+        status = build_camconnect_desktop(desktop_project_dir, desktop_output_dir)
+        if status != 0:
+            return status
+
+        repo_url = "https://github.com/schellingb/UnityCapture.git"
+        repo_name = repo_url.rsplit("/", maxsplit=1)[1].rstrip(".git")
+        repo_dir = os.path.join(output_dir, repo_name)
+
+        if not os.path.exists(repo_dir):
+            logging.info(f"Cloning {repo_name} repository...")
+            status = subprocess.call(["git", "clone", repo_url, repo_dir], shell=True)
+
+            if status != 0:
+                logging.error(f"Failed to clone {repo_name} repository.")
+                return status
+
+        logging.info(f"Copying {repo_name} files...")
+        shutil.copytree(
+            os.path.join(repo_dir, "Install"),
+            os.path.join(desktop_output_dir, "filters")
+        )
+
+        app_version = get_version_from_pubspec(
+            os.path.join(desktop_project_dir, "pubspec.yaml")
+        )
+        return create_setup_installer(os.path.join(script_dir, "setup.iss"), app_version)
+
+    return 0  # success.
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
